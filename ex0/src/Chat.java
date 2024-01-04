074c7a9f348929950f27911a4ab04d9ea597b271
tomerm1 <tomer.meidan1@mail.huji.ac.il>
2024-01-04 13:30:58 +0200

    ex0 step 4

--- ex0/src/Chat.java
+++ ex0/src/Chat.java
@@ -9,8 +9,11 @@ public class Chat {
         ChatterBot[] chatterBots = new ChatterBot[2];
         String[] bot1IllegalReplies = {"what", "say I should say"};
         String[] bot2IllegalReplies = {"whaaat", "say say"};
-        chatterBots[0] = new ChatterBot("Roni", bot1IllegalReplies);
-        chatterBots[1] = new ChatterBot("Amit", bot2IllegalReplies);
+        String placeHolder = ChatterBot.PLACEHOLDER_FOR_REQUESTED_PHRASE;
+        String[] bot1LegalReplies = {"You want me to say " + placeHolder + ", do you? alright: " + placeHolder};
+        String[] bot2LegalReplies = {"say " + placeHolder + "? okay: " + placeHolder};
+        chatterBots[0] = new ChatterBot("Roni", bot1IllegalReplies, bot1LegalReplies);
+        chatterBots[1] = new ChatterBot("Amit", bot2IllegalReplies, bot2LegalReplies);
 
         String statement = FIRST_STATEMENT;
         Scanner scanner = new Scanner(System.in);
--- ex0/src/ChatterBot.java
+++ ex0/src/ChatterBot.java
@@ -12,27 +12,43 @@ import java.util.*;
  */
 class ChatterBot {
 	static final String REQUEST_PREFIX = "say ";
+	static final String PLACEHOLDER_FOR_REQUESTED_PHRASE = "<phrase>";
 
 	Random rand = new Random();
 	String[] repliesToIllegalRequest;
+	String[] legalRequestsReplies;
 
 	String name;
 
-	ChatterBot(String name, String[] repliesToIllegalRequest) {
+	ChatterBot(String name, String[] repliesToIllegalRequest, String[] legalRequestsReplies ) {
 		this.name = name;
 		this.repliesToIllegalRequest = new String[repliesToIllegalRequest.length];
 		for(int i = 0 ; i < repliesToIllegalRequest.length ; i = i+1) {
 			this.repliesToIllegalRequest[i] = repliesToIllegalRequest[i];
 		}
+
+		this.legalRequestsReplies = new String[legalRequestsReplies.length];
+		for(int i = 0 ; i < legalRequestsReplies.length ; i = i+1) {
+			this.legalRequestsReplies[i] = legalRequestsReplies[i];
+		}
 	}
 
 	String replyTo(String statement) {
 		if(statement.startsWith(REQUEST_PREFIX)) {
-			//we don’t repeat the request prefix, so delete it from the reply
-			return statement.replaceFirst(REQUEST_PREFIX, "");
+
+			return replyToLegalRequest(statement);
 		}
 		return replyToIllegalRequest(statement);
 	}
+
+	String replyToLegalRequest(String statement) {
+		String phrase = statement;
+		int randomIndex = rand.nextInt(this.legalRequestsReplies.length);
+		String responsePattern = this.legalRequestsReplies[randomIndex];
+		String reply = responsePattern.replaceAll(PLACEHOLDER_FOR_REQUESTED_PHRASE, phrase);
+		return reply;
+	}
+
 
 	String replyToIllegalRequest(String statement) {
 		int randomIndex = rand.nextInt(repliesToIllegalRequest.length);
