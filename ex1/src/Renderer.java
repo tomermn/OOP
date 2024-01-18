f8302f50a1f5e42af05079584788940d15e8d64b
tomerm1 <tomer.meidan1@mail.huji.ac.il>
2024-01-18 16:15:12 +0200

    create Renderer interface and VoidRenderer class

--- ex1/src/Renderer.java
+++ ex1/src/Renderer.java
@@ -1,3 +1,9 @@
+
+/**
+ * The Renderer interface defines a contract for classes responsible for rendering a game board.
+ * Implementing classes should provide a concrete implementation of the renderBoard method
+ * to visually represent the state of a game board.
+ */
 public interface Renderer {
     void renderBoard(Board board);
 }
--- ex1/src/VoidRenderer.java
+++ ex1/src/VoidRenderer.java
@@ -1,2 +1,6 @@
-public class VoidRenderer {
+/**
+ * VoidRenderer is a simple implementation of the Renderer interface that does not perform any rendering.
+ */
+public class VoidRenderer implements Renderer {
+    public void renderBoard(Board board){}
 }
