35c1dc3572ad25f215f0a00982900b5c43f7e82b
tomermn <142005546+tomermn@users.noreply.github.com>
2024-03-23 10:22:54 +0200

    Delete oop_ex2_new directory

--- oop_ex2_new/src/bricker/brick_strategies/CollisionStrategy.java
+++
@@ -1,8 +0,0 @@
-package bricker.brick_strategies;
-
-import danogl.GameObject;
-import danogl.collisions.GameObjectCollection;
-
-public interface CollisionStrategy {
-    public void onCollision(GameObject obj1, GameObject obj2);
-}
